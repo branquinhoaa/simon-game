e4a7f395a8a152d1a2964393c189e1eeefe05566
branquinhoaa <amanda.alves15@gmail.com>
2016-06-15 17:28:55 -0300

    simon game to ooversion - with corrections

--- script.js
+++ script.js
@@ -1,170 +1,170 @@
 $(document).ready(function(){
-  var ui=new UI();
-  ui.turnOnOff();  
+	var ui=new UI();
+	ui.turnOnOff();  
 });
 
-function UI(){  
-}
-
-UI.prototype.turnOnOff = function(){
-  var self=this;
-  $('.offBtn').on('click', function(){
-    $('.offBtn').toggleClass('onBtn');
-    if ($('.offBtn').hasClass('onBtn')){ 
-      var player = new Player();    
-      player.bindPlayerEvents();
-      self.setBtnFun(false);
-    } else{
-      self.setBtnFun('disabled');
-      $('#number').html(' ');
-      $('#strictActive').removeClass('active');
-      location.reload(true);
-    }
-  });
-}
-
-UI.prototype.setBtnFun=function(arg){
-  $('.starter').attr('disabled',arg);
-}
-
 function Game(typeofGame){
-  this.num=1;
-  this.typeofGame = typeofGame;
-  this.rightSequence=[];
-  this.personSequence=[];    
-  this.order=0;
-  this.maxRounds=20;
-  this.allColors = ['green','red','blue','yellow'];
+	this.typeofGame = typeofGame;
+	this.rightSequence=[];
+	this.personSequence=[];    
+	this.allColors = ['green','red','blue','yellow'];     
+	this.order=0;
 }
 
-
-Game.prototype.startGame = function (){
-  var numColors = this.allColors.length;
-  var random=Math.floor((Math.random() * numColors));
-  this.rightSequence.push(this.allColors[random]);
-  this.changeColorDisplay('none');
-  this.displaySequence();
-}
-
-
-Game.prototype.lightSound = function (cor,callback){
-  $('#'+cor).addClass('active');
-  var storeColInf ={
-    'red': 1,
-    'green': 2,
-    'blue': 3,
-    'yellow': 4   
-  };
-  this.playAudio(cor, storeColInf[cor], callback).play(); 
-}
-
-Game.prototype.displaySequence  = function (){
-  var timeWait=800;
-
-  $('#number').html(this.num);
-  var counter=0; 
-  for (var i in this.rightSequence){
-    var color=this.rightSequence[i];
-    timing(this.lightSound.bind(this), timeWait * ++counter, color);
-  }
-  timing(this.changeColorDisplay.bind(this), timeWait*counter+1, 'auto');
-
-  function timing(callback, time, arg){
-    setTimeout(function(){
-      callback(arg);
-    }, time);
-  }
-}
-
-Game.prototype.playAudio =function(color,number, callback){
-  return  new Howl({
-    urls: ['https://s3.amazonaws.com/freecodecamp/simonSound'+number+'.mp3'],
-    volume: 0.5,
-    onend: function() {
-      $('#'+color).removeClass('active');
-      if (callback){
-        callback();
-      }
-    }
-  });  
-}
-
-Game.prototype.changeColorDisplay=function(arg){
-  $('span').css('pointer-events', arg);
-}
-
-Game.prototype.verify = function (cor){
-  var timeRestart=2000;
-  var self=this;
-  if (this.rightSequence[this.order-1]!==cor){
-    this.changeColorDisplay('none');
-    if(this.typeofGame=='strict'){        
-      $('#number').html('loser');
-      setTimeout(function(){
-       self.reset('all');
-       self.startGame();
-      },timeRestart);
-      return;
-    } else{  
-      this.reset();
-      $('#number').trigger('error');
-      return;
-    }
-  }
-  if (this.rightSequence.length==this.personSequence.length){
-    if(this.rightSequence.length>=this.maxRounds){
-      $('#number').html('winner!');
-      $('#'+this.typeofGame).attr('disabled', false);
-    } else {
-      this.num++;
-      this.reset();
-      this.startGame();  
-    } 
-  }
-}
-
-Game.prototype.bindGameEvents=function (){
-  var self=this;
-  $('#number').on('error', function(){
-    $('#number').html('--');
-    setTimeout(function(){
-      self.displaySequence();
-      $('#number').html(self.num);
-    },1500);
-  });  
-
-  $('.color').on('click', function(){
-    var idClicked = $(this).attr('id');
-    self.order++;
-    self.lightSound(''+idClicked+'');
-    self.personSequence.push(idClicked);
-    self.verify(''+idClicked+'');
-  });
+Game.prototype.sortColor = function (){
+	var numColors = this.allColors.length;
+	var random=Math.floor((Math.random() * numColors));
+	this.rightSequence.push(this.allColors[random]);
+	return this.rightSequence;
 }
 
 Game.prototype.reset = function(all){
-  this.personSequence=[];
-  this.order=0;
-  if (all){
-    this.num=1;
-    this.rightSequence=[]; 
-  }
+	this.personSequence=[];
+	this.order=0;
+	if (all){
+		this.rightSequence=[]; 
+	}
 }
 
-function Player (){
+Game.prototype.isRight = function(color){
+	return (this.rightSequence[this.order-1]===color);	
 }
 
-Player.prototype.bindPlayerEvents = function(){
-  var self = this;
-  $('.starter').on('click', function(){
-    $('.starter').attr('disabled','disabled');
-    var typeofGame = $(this).attr('id')
-    if (typeofGame=='strict'){
-      $('#strictActive').addClass('active');
-    }
-    var game = new Game (typeofGame);
-    game.bindGameEvents();
-    game.startGame(); 
-  }); 
+function UI(){  
+	this.game;
+	this.num=1;
+	this.maxRounds=20; 
+}
+
+UI.prototype.turnOnOff = function(){
+	var self=this;
+	$('.offBtn').on('click', function(){
+		$('.offBtn').toggleClass('onBtn');
+		if ($('.offBtn').hasClass('onBtn')){ 
+			self.changeColorDisplay('none');
+			self.bindGameEvents();
+			self.setBtnFun(false);
+		} else{
+			self.setBtnFun('disabled');
+			$('#number').html(' ');
+			$('#strictActive').removeClass('active');
+			location.reload(true);
+		}
+	});
+}
+
+UI.prototype.setBtnFun=function(arg){
+	$('.starter').attr('disabled',arg);
+}
+
+UI.prototype.lightSound = function (cor,callback){          
+	$('#'+cor).addClass('active');
+	var storeColInf ={
+		'red': 1,
+		'green': 2,
+		'blue': 3,
+		'yellow': 4   
+	};
+	this.playAudio(cor, storeColInf[cor], callback).play(); 
+}
+
+UI.prototype.displaySequence  = function (sequence){                
+	var timeWait=800;
+
+	$('#number').html(this.num);
+	var counter=0; 
+	for (var i in sequence){
+		var color=sequence[i];
+		timing(this.lightSound.bind(this), timeWait * ++counter, color);
+	}
+	timing(this.changeColorDisplay.bind(this), timeWait*counter+1, 'auto');
+
+	function timing(callback, time, arg){
+		setTimeout(function(){
+			callback(arg);
+		}, time);
+	}
+}
+
+UI.prototype.playAudio =function(color,number, callback){                
+	return  new Howl({
+		urls: ['https://s3.amazonaws.com/freecodecamp/simonSound'+number+'.mp3'],
+		volume: 0.5,
+		onend: function() {
+			$('#'+color).removeClass('active');
+			if (callback){
+				callback();
+			}
+		}
+	});  
+}
+
+UI.prototype.changeColorDisplay=function(arg){ 
+	$('span').css('pointer-events', arg);
+}
+
+UI.prototype.verify = function (cor){     
+	var timeRestart=2000;
+	var self=this;
+	if (!this.game.isRight(cor)){  
+		this.changeColorDisplay('none');
+		if(this.game.typeofGame=='strict'){    
+			$('#number').html('loser');
+			setTimeout(function(){
+				self.game.reset('all');
+				self.num=1;
+				self.proceedGame();
+			},timeRestart);
+			return;
+		} else{  
+			this.game.reset();
+			$('#number').trigger('error');
+			return;
+		}
+	}
+	if (this.game.rightSequence.length==this.game.personSequence.length){
+		if(this.game.rightSequence.length>=this.maxRounds){
+			$('#number').html('winner!');
+			$('#'+this.game.typeofGame).attr('disabled', false);
+		} else {
+			this.num++;
+			this.game.reset();
+			this.proceedGame();  
+		} 
+	}
+}
+
+UI.prototype.bindGameEvents=function (){
+	var self=this;
+	$('#number').on('error', function(){
+		$('#number').html('--');
+		setTimeout(function(){
+			self.displaySequence(self.game.rightSequence);
+			$('#number').html(self.num);
+		},1500);
+	});  
+
+	$('.color').on('click', function(){
+		var idClicked = $(this).attr('id');
+		self.game.order++;
+		self.lightSound(''+idClicked+'');
+		self.game.personSequence.push(idClicked);
+		self.verify(''+idClicked+'');
+	});
+	$('.starter').on('click', function(){
+		self.setBtnFun('disabled');
+		var typeofGame = $(this).attr('id')
+		if (typeofGame=='strict'){
+			$('#strictActive').addClass('active');
+		}
+		self.game= new Game (typeofGame);
+		self.proceedGame();
+	}); 
+}
+
+UI.prototype.proceedGame= function(){
+	var seq=this.game.sortColor(); 
+	this.changeColorDisplay('none');
+	this.displaySequence(seq);
 }
 
