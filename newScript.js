2eaa725495bcc2c2f57a706368c6705d1fe89256
branquinhoaa <amanda.alves15@gmail.com>
2016-06-15 11:37:24 -0300

    simon game to ooversion

---
+++ newScript.js
@@ -0,0 +1,20 @@
+function OnOffBtn(){
+    stayOn:function(){
+    //things to happen when button is on
+    }
+    
+    stayOff:function(){
+    //things to happen when btn is off;
+    }
+    
+}
+function BtnPlay(witchBtn){
+    this.typeOfGame=witchBtn; //start or strict 
+    playBtn:function(witchBtn){
+        
+    }
+} //prototype of start/strict
+
+function counter(){
+    
+} // classe contador
--- script.js
+++ script.js
@@ -1,148 +1,170 @@
 $(document).ready(function(){
-  var num=1,
-      random,
-      btnClicked,
-      typeofGame,
-      allColors=['green','red','blue','yellow'],
-      rightSequence=[],
-      personSequence=[],
-      order=0,
-      timeWait=800,
-      timeRestart=2000,
-      numColors=4,
-      maxRounds=20,
-      storeColInf ={
-        'red': 1,
-        'green': 2,
-        'blue': 3,
-        'yellow': 4   
-      };
+  var ui=new UI();
+  ui.turnOnOff();  
+});
 
-  changeColorDisplay('none');
-  setBtnFun('disabled');
+function UI(){  
+}
 
-  $('.starter').on('click', function(){
-    typeofGame = $(this).attr('id')
-    if (typeofGame=='strict'){
-      $('#strictActive').addClass('strictActive');
-    }
-    setBtnFun('disabled');
-    continueGame();
-  });  
-
-  $('.color').on('click', function(){
-    var idClicked = $(this).attr('id');
-    order++;
-    lightSound(''+idClicked+'');
-    personSequence.push(idClicked);
-    verify(''+idClicked+'');  
-  });
-
-  $('#number').on('error', function(event, callback){
-    $('#number').html('--');
-    setTimeout(function(){
-      callback();
-      $('#number').html(num);
-    },1500);
-  });
-
+UI.prototype.turnOnOff = function(){
+  var self=this;
   $('.offBtn').on('click', function(){
     $('.offBtn').toggleClass('onBtn');
-    if ($('.offBtn').hasClass('onBtn')){ //se tiver on
-      setBtnFun(false);
+    if ($('.offBtn').hasClass('onBtn')){ 
+      var player = new Player();    
+      player.bindPlayerEvents();
+      self.setBtnFun(false);
     } else{
-      changeColorDisplay('none');
-      setBtnFun('disabled')
-      reset('all');
+      self.setBtnFun('disabled');
       $('#number').html(' ');
-      $('#strictActive').removeClass('strictActive');
+      $('#strictActive').removeClass('active');
+      location.reload(true);
     }
   });
+}
 
-  function playAudio (color,number, callback){
-    return  new Howl({
-      urls: ['https://s3.amazonaws.com/freecodecamp/simonSound'+number+'.mp3'],
-      volume: 0.5,
-      onend: function() {
-        $('#'+color).removeClass(''+color+'active');
-        if (callback){
-          callback();
-        }
-      }
-    });  
-  }
+UI.prototype.setBtnFun=function(arg){
+  $('.starter').attr('disabled',arg);
+}
 
-  function continueGame(){
-    random=Math.floor((Math.random() * numColors));
-    rightSequence.push(allColors[random]);
-    changeColorDisplay('none');
-    doIt();
-  }
+function Game(typeofGame){
+  this.num=1;
+  this.typeofGame = typeofGame;
+  this.rightSequence=[];
+  this.personSequence=[];    
+  this.order=0;
+  this.maxRounds=20;
+  this.allColors = ['green','red','blue','yellow'];
+}
 
-  function doIt(){
-    $('#number').html(num);
-    var counter=0; 
-    for (var i in rightSequence){
-      var color=rightSequence[i];
-      timing(lightSound, timeWait * ++counter, color);
-    }
-    timing(changeColorDisplay, timeWait*counter+1, 'auto');
+
+Game.prototype.startGame = function (){
+  var numColors = this.allColors.length;
+  var random=Math.floor((Math.random() * numColors));
+  this.rightSequence.push(this.allColors[random]);
+  this.changeColorDisplay('none');
+  this.displaySequence();
+}
+
+
+Game.prototype.lightSound = function (cor,callback){
+  $('#'+cor).addClass('active');
+  var storeColInf ={
+    'red': 1,
+    'green': 2,
+    'blue': 3,
+    'yellow': 4   
+  };
+  this.playAudio(cor, storeColInf[cor], callback).play(); 
+}
+
+Game.prototype.displaySequence  = function (){
+  var timeWait=800;
+
+  $('#number').html(this.num);
+  var counter=0; 
+  for (var i in this.rightSequence){
+    var color=this.rightSequence[i];
+    timing(this.lightSound.bind(this), timeWait * ++counter, color);
   }
+  timing(this.changeColorDisplay.bind(this), timeWait*counter+1, 'auto');
 
   function timing(callback, time, arg){
     setTimeout(function(){
       callback(arg);
     }, time);
   }
+}
 
-  function lightSound(cor, callback){
-    $('#'+cor).addClass(''+cor+'active');
-    playAudio(cor, storeColInf[cor], callback).play(); 
-  }
-
-  function verify(cor){
-    if (rightSequence[order-1]!==cor){
-      changeColorDisplay('none');
-      if(typeofGame=='strict'){  //restart        
-        $('#number').html('loser');
-        setTimeout(function(){
-          reset('all');
-          continueGame();
-        },timeRestart);
-        return;
-      } else{  //repeat sequence
-        reset();
-        $('#number').trigger('error',[doIt]);
-        return;
+Game.prototype.playAudio =function(color,number, callback){
+  return  new Howl({
+    urls: ['https://s3.amazonaws.com/freecodecamp/simonSound'+number+'.mp3'],
+    volume: 0.5,
+    onend: function() {
+      $('#'+color).removeClass('active');
+      if (callback){
+        callback();
       }
     }
-    if (rightSequence.length==personSequence.length){
-      if(rightSequence.length>=maxRounds){
-        $('#number').html('winner!');
-        $('#'+typeofGame).attr('disabled', false);
-      } else {
-        num++;
-        reset();
-        continueGame();  
-      } 
+  });  
+}
+
+Game.prototype.changeColorDisplay=function(arg){
+  $('span').css('pointer-events', arg);
+}
+
+Game.prototype.verify = function (cor){
+  var timeRestart=2000;
+  var self=this;
+  if (this.rightSequence[this.order-1]!==cor){
+    this.changeColorDisplay('none');
+    if(this.typeofGame=='strict'){        
+      $('#number').html('loser');
+      setTimeout(function(){
+       self.reset('all');
+       self.startGame();
+      },timeRestart);
+      return;
+    } else{  
+      this.reset();
+      $('#number').trigger('error');
+      return;
     }
   }
+  if (this.rightSequence.length==this.personSequence.length){
+    if(this.rightSequence.length>=this.maxRounds){
+      $('#number').html('winner!');
+      $('#'+this.typeofGame).attr('disabled', false);
+    } else {
+      this.num++;
+      this.reset();
+      this.startGame();  
+    } 
+  }
+}
 
-  function reset (all){
-    personSequence=[];
-    order=0;
-    if (all){
-      num=1;
-      rightSequence=[]; 
+Game.prototype.bindGameEvents=function (){
+  var self=this;
+  $('#number').on('error', function(){
+    $('#number').html('--');
+    setTimeout(function(){
+      self.displaySequence();
+      $('#number').html(self.num);
+    },1500);
+  });  
+
+  $('.color').on('click', function(){
+    var idClicked = $(this).attr('id');
+    self.order++;
+    self.lightSound(''+idClicked+'');
+    self.personSequence.push(idClicked);
+    self.verify(''+idClicked+'');
+  });
+}
+
+Game.prototype.reset = function(all){
+  this.personSequence=[];
+  this.order=0;
+  if (all){
+    this.num=1;
+    this.rightSequence=[]; 
+  }
+}
+
+function Player (){
+}
+
+Player.prototype.bindPlayerEvents = function(){
+  var self = this;
+  $('.starter').on('click', function(){
+    $('.starter').attr('disabled','disabled');
+    var typeofGame = $(this).attr('id')
+    if (typeofGame=='strict'){
+      $('#strictActive').addClass('active');
     }
-  }
+    var game = new Game (typeofGame);
+    game.bindGameEvents();
+    game.startGame(); 
+  }); 
+}
 
-  function changeColorDisplay(arg){
-    $('span').css('pointer-events', arg);
-  }
-
-  function setBtnFun(arg){
-    $('.starter').attr('disabled',arg);
-  }
-
-});
